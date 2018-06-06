1a1664069cc6d8bbd928a350ebd16215c9c72f8c
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-06-06 13:24:55 +0100

    ch.10 - refactoring

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,7 +3,6 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks in an array' do
-
       # Add the test data
       Bookmark.create(url: "http://makersacademy.com")
       Bookmark.create(url: "http://destroyallsoftware.com")
@@ -14,7 +13,6 @@ describe Bookmark do
         'http://destroyallsoftware.com',
         'http://google.com'
       ]
-
       expect(Bookmark.all).to eq expected_bookmarks
     end
   end
@@ -22,8 +20,12 @@ describe Bookmark do
   describe '.create' do
     it 'creates a new bookmark' do
       Bookmark.create(url: 'http://www.testbookmark.com')
-
       expect(Bookmark.all).to include 'http://www.testbookmark.com'
+    end
+
+    it 'does not create a new bookmark if the URL is not valid' do
+      Bookmark.create(url: 'not a real bookmark')
+      expect(Bookmark.all).not_to include 'not a real bookmark'
     end
   end
 end
